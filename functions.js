be9de80437294dca89d962c74639c9ca960a1380
David Helebrant <rtg077@gmail.com>
2020-08-06 19:13:53 +0200

    feat: add background, topmenu, h1 margin-left

--- functions.js
+++ functions.js
@@ -20,19 +20,15 @@ toggleItem(document.querySelectorAll('.link'));
 function myZoom() {
 var current = document.getElementById("topNav");
 if (current.classList.contains('small') === true) {
-    console.log('there was small class bro');
     current.classList.remove('small');
 } else {
-    console.log('there was NOT small class bro');
     current.classList.add('small')
 }
 
 var topMenu = document.getElementById("wrapper");
 if (topMenu.classList.contains('topShrinked') === true) {
-    console.log('topMenu there was small class bro');
     topMenu.classList.remove('topShrinked');
 } else {
-    console.log('topMenu there was NOT small class bro');
     topMenu.classList.add('topShrinked')
 }
                 
@@ -56,34 +52,7 @@ function openPage(x) {
     }
     location.href = page;
 }
-function myScroll(x) {
-    
-    var scrollTo;
-    switch (x) {
-    
-    case 0:
-    scrollTo = "01predprojektovapriprava";
-    break;
-    
-    case 1:
-    scrollTo = "02projektovani";
-    break;
 
-    case 2:
-    scrollTo = "03realizacestaveb";
-    break;
-
-    case 3:
-    scrollTo = "04stavebnidozory";
-    break;
-    
-    }
-
-    console.log (scrollTo);
-
-    var elmnt = document.getElementById(scrollTo);
-    elmnt.scrollIntoView(false);
-}
 
 function myScroll(x) {
     
@@ -127,5 +96,5 @@ function myScroll(x) {
     console.log (scrollTo);
 
     var elmnt = document.getElementById(scrollTo);
-    elmnt.scrollIntoView(false);
+    elmnt.scrollIntoView(true);
 }
